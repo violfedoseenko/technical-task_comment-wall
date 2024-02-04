d588e38e723e0e8c0d6dfe8c24ec63b3009ef995
Violetta <viol.fedoseenko@yandex.ru>
2024-02-05 01:20:05 +0300

    Updates

--- src/lib/date.ts
+++
@@ -1,7 +0,0 @@
-export function subtractHours(date: Date, numOfHours: number) {
-    const dateCopy = new Date(date.getTime());
-
-    dateCopy.setHours(dateCopy.getHours() - numOfHours);
-
-    return dateCopy;
-}
